08578b74e2e7f2d639520132394969ad105f5975
Ajithkumar-M01 <ajithkumaran.srk@gmail.com>
2024-11-09 23:40:48 +0530

    added more details

--- server.js
+++ server.js
@@ -23,9 +23,12 @@ mongoose
 
 const superheroSchema = new mongoose.Schema({
     superheroName: {type: String, required: true},
-    originalName: {type: String, required: true},
-    abilities: {type: String},
+    originalName: { type: String, required: true },
     description: {type: String},
+    abilities: { type: String },
+    weakness: { type: String },
+    backstory: { type: String },
+    reason: { type: String },
 })
 
 const Superhero = mongoose.model("Superhero", superheroSchema)
@@ -43,8 +46,11 @@ app.post("/api/superheroes", async (req, res) => {
     const newSuperhero = new Superhero({
         superheroName: req.body.superheroName,
         originalName: req.body.originalName,
-        abilities: req.body.abilities,
         description: req.body.description,
+        abilities: req.body.abilities,
+        weakness: req.body.weakness,
+        backstory: req.body.backstory,
+        reason: req.body.reason
     })
 
     try {
